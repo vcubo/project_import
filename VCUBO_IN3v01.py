8d0946c482e7f8442929cc669e8aaa972e8838cd
Facundo <facundo.bassi@vcubo.co>
2021-10-27 15:17:35 -0300

    Simpliflying import UI

--- VCUBO_IN3v01.py
+++ VCUBO_IN3v01.py
@@ -7,11 +7,10 @@ import io
 import streamlit as st
 
 st.set_page_config(page_title='Project import - vcubo', page_icon='favicon.png',menu_items={'Get Help': 'https://vcubo.co/contact','Report a bug': "https://vcubo.co/contact",'About': " Unbiased risk ananlysis. *vcubo*"})
+
 @st.cache(allow_output_mutation=True, hash_funcs={"_thread.RLock": lambda _: None})
 def init_connection():
     return psycopg2.connect(**st.secrets["postgres_prod"])
-
-#conn2 = init_connection()
 
 @st.cache(ttl=600)
 def run_query(query):
@@ -76,44 +75,50 @@ def read_uploaded(project_id):
 #    upload_template = st.button('UPLOAD TEMPLATE')
 #    if upload_template:
 #        write_blob(st.session_state.path_to_file, st.session_state.template_name)
-st.subheader('DOWNLOAD TEMPLATE')
-with st.expander('', expanded=False):
-    files_dict = {'LOGO':'vcubo_logo.png', 'PROJECT IMPORT TEMPLATE':'vcubo_project_import.xlsm'}
-    file_sel = st.selectbox('DOWNLOAD FILE', ['PROJECT IMPORT TEMPLATE'] )
-    st.download_button('DOWNLOAD TEMPLATE',
-        data=read_blob(files_dict[file_sel]),
-        file_name=files_dict[file_sel])
+
+
+#st.subheader('DOWNLOAD TEMPLATE')
+#with st.expander('', expanded=False):
+    #files_dict = {'LOGO':'vcubo_logo.png', 'PROJECT IMPORT TEMPLATE':'vcubo_project_import.xlsm'}
+    #file_sel = st.selectbox('DOWNLOAD FILE', ['PROJECT IMPORT TEMPLATE'] )
+    #st.download_button('DOWNLOAD TEMPLATE',
+        #data=read_blob(files_dict[file_sel]),
+        #file_name=files_dict[file_sel])
 
     #st.markdown('***')
-    decrypted =read_blob('vcubo_project_import.xlsm')
+    #decrypted =read_blob('vcubo_project_import.xlsm')
     #st.write(type(decrypted))
-    to_read = io.BytesIO()
-    to_read.write(decrypted)
+    #to_read = io.BytesIO()
+    #to_read.write(decrypted)
     #st.write(to_read)
-    to_read.seek(0)
-    test_excel = pd.read_excel(to_read, sheet_name=2, header=0)
+    #to_read.seek(0)
+    #test_excel = pd.read_excel(to_read, sheet_name=2, header=0)
     #preview = st.button('PREVIEW')
     #if preview:
     #    test_excel
     #st.dataframe(read_blob('vcubo_project_import.xlsx'))
-st.subheader('IMPORT PROJECT DATA')
-with st.expander('', expanded=False):
-    #st.session_state.path_to_upload =st.text_input('IMPORT FILE PATH:')
+st.download_button('DOWNLOAD IMPORT TEMPLATE',
+    data=read_blob('vcubo_project_import.xlsm'),
+    file_name='vcubo_project_import.xlsm')
+
+#st.subheader('IMPORT PROJECT DATA')
+with st.expander('IMPORT PROJECT FILE', expanded=True):
     uploaded_file = st.file_uploader('SELECT FILE', type='xlsm')
 
     st.session_state.project_id = st.text_input('PROJECT ID:')
+    st.session_state.project_desc = st.text_area('BRIEF PROJECT DESCRIPTION', max_chars=200)
     st.session_state.pr_ids_main = pd.read_sql(f"SELECT l1_id  FROM pr_main", conn2)
     st.session_state.pr_ids_upl = pd.read_sql(f"SELECT project_id  FROM pr_imported_main", conn2)
     if st.session_state.project_id =='':
-        st.warning(f'ENTER PROJECT ID')
+        st.warning(f'ENTER UNIQUE PROJECT ID')
     elif st.session_state.project_id in st.session_state.pr_ids_main['l1_id'].tolist():
         st.warning(f'PROJECT ID {st.session_state.project_id} ALREADY EXISTS IN MAIN DATABASE')
     elif st.session_state.project_id in st.session_state.pr_ids_upl['project_id'].tolist():
         st.warning(f'PROJECT ID {st.session_state.project_id} ALREADY IMPORTED (WAITING FOR VALIDATION)')
     else:
         upload_project_file = st.button('UPLOAD PROJECT')
-        type(uploaded_file)=="<class 'NoneType'>"
-        st.write(type(uploaded_file))
+        #type(uploaded_file)=="<class 'NoneType'>"
+        st.warning(f'pre-uploaded file type: {type(uploaded_file)}')
         if (upload_project_file and type(uploaded_file)!='NoneType'):
             upload_blob(st.session_state.project_id, uploaded_file.getvalue())
             time.sleep(2)
